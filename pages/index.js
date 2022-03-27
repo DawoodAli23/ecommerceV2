55f94b27cf30ebb0ed1b2d97ceb9a5d537bbcc76
Dawood Ali <59256614+DawoodAli23@users.noreply.github.com>
2022-03-27 22:25:38 +0500

    added component folder

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Test from '../components/Test'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-800">
-      Hello world!
-    </h1>
+    <Test/>
   )
 }
